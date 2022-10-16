f8847a9565145c76da6641caab4f2ca19e607172
Alhladkiy <Alhladkiy@gmail.com>
2022-10-16 23:19:57 +0300

    feat: update CSVFileImport

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -25,21 +25,24 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
 
   const uploadFile = async () => {
     console.log("uploadFile to", url);
+    if (!file) {
+      return;
+    }
     const response = await axios({
       method: "GET",
       url,
       params: {
-        name: encodeURIComponent(file!.name),
+        name: encodeURIComponent(file.name),
       },
     });
-    console.log("File to upload: ", file!.name);
+    console.log("File to upload: ", file.name);
     console.log("Uploading to: ", response.data);
     const result = await fetch(response.data, {
       method: "PUT",
       body: file,
     });
     console.log("Result: ", result);
-    setFile(file!);
+    setFile(undefined);
   };
   return (
     <Box>
