f8736012231155610691266a383af6819afb0c10
Alhladkiy <Alhladkiy@gmail.com>
2022-11-06 22:54:59 +0300

    feat: add cart api link

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://7bx9cxw31g.execute-api.eu-west-1.amazonaws.com/dev/import",
   bff: "https://rq0j3k4zl0.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "http://alhladkiy-cart-api-dev.eu-west-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
